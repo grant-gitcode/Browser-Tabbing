f98821281c58b1e270810ca164a0d2d29a2246ff
grant-gitcode <grant.tbird@gmail.com>
2018-02-19 00:02:01 -0600

    Commented code, although this maybe isn't a super necessary task.

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
+//Initialize isKey to false so it doesn't scroll on its own randomly.
 chrome.storage.local.set({"isKey" : false});
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   //Gets currently active tab.
   chrome.tabs.query({"active" : true, "lastFocusedWindow" : true}, function(tabs) {
+    //If we scroll down, get index of tab to the left. Else, get tab index to the right.
     if(request.direction > 0) index = tabs[0].index - 1;
     if(request.direction < 0) index = tabs[0].index + 1;
+
+    //Make sure we didn't try to scroll past the 0th tab.
     if(request.direction != 0 && index > -1) {
       //Gets the next tab over to be scrolled to.
       chrome.tabs.query({"index" : index, "lastFocusedWindow" : true}, function(tabs) {
+        //Checks to make sure we didn't scroll past the nth tab.
         if(tabs[0] != undefined) chrome.tabs.update(tabs[0].id, {"active" : true});
       });
     }
--- insert.js
+++ insert.js
@@ -1,17 +1,21 @@
 var isKey;
 
+//If the shift key is down, set isKey to true.
 window.addEventListener("keydown", function(key) {
   chrome.storage.local.set({"isKey" : key.shiftKey});
 });
 
+//If a key is released, set isKey to false.
 window.addEventListener("keyup", function(key) {
   chrome.storage.local.set({"isKey" : false});
 });
 
+//On scroll, get the value of isKey, check if there is wheel movement, and isKey is true.
 window.addEventListener("wheel", function(event) {
   chrome.storage.local.get("isKey",function(value) {
     isKey = value.isKey;
     if(event.deltaY != null && isKey) {
+      //If true, pass message to background JS file, giving it the direction of the mouse scroll.
       chrome.runtime.sendMessage({"direction" : event.deltaY});
     }
   });
